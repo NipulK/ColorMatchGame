c106876788ffff5749895c4c54b97136d65f6753
NipulK <168514285+NipulK@users.noreply.github.com>
2026-01-22 11:46:37 +0530

    Add new Home page(create new sw file) and modify ContetView.sw file

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -1,8 +1,7 @@
 import SwiftUI
 
-
 struct ContentView: View {
     var body: some View {
-        LevelSelectionView()
+        HomeView()
     }
 }
---
+++ Views/HomeView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct HomeView: View {
+
+    @State private var goToDashboard = false
+
+    var body: some View {
+
+        NavigationView {
+
+            VStack(spacing: 30) {
+
+                Text("Color Match Game")
+                    .font(.largeTitle)
+                    .bold()
+
+                // PLAY BUTTON
+                NavigationLink(
+                    destination: LevelSelectionView(),
+                    isActive: $goToDashboard
+                ) {
+                    Button("PLAY") {
+                        goToDashboard = true
+                    }
+                    .font(.title2)
+                    .padding()
+                    .frame(width: 200)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+                }
+
+                // QUIT BUTTON
+                Button("QUIT") {
+                    exit(0) // academic/demo purpose
+                }
+                .font(.title2)
+                .padding()
+                .frame(width: 200)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(12)
+            }
+        }
+    }
+}
+
