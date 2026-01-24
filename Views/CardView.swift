fa8ddc622db6adb7104d7a7dfc05cd38c4aa16c8
NipulK <168514285+NipulK@users.noreply.github.com>
2026-01-24 11:23:05 +0530

    Update the CardView.sw file

--- Views/CardView.swift
+++ Views/CardView.swift
@@ -8,53 +8,25 @@ struct CardView: View {
 
         ZStack {
 
-            
-            // BACK SIDE
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.blue)
-                .overlay(
-                    Text("?")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                )
+            RoundedRectangle(cornerRadius: 14)
+                .fill(Color.blue.opacity(0.9))
                 .opacity(card.isFaceUp ? 0 : 1)
 
-            // FRONT SIDE
-            RoundedRectangle(cornerRadius: 10)
+            RoundedRectangle(cornerRadius: 14)
                 .fill(card.color)
                 .opacity(card.isFaceUp ? 1 : 0)
-
         }
         .frame(height: 70)
-
-        //  REAL 3D FLIP  card
         .rotation3DEffect(
             .degrees(card.isFaceUp ? 180 : 0),
             axis: (x: 0, y: 1, z: 0)
         )
-
-        
-        .animation(.easeInOut(duration: 0.4),
-                   value: card.isFaceUp)
-
-        .shadow(radius: 3)
-        
-        .frame(height: 70)
-
-        .rotation3DEffect(
-            .degrees(card.isFaceUp ? 180 : 0),
-            axis: (x: 0, y: 1, z: 0)
-        )
-
-        .animation(.easeInOut(duration: 0.4), value: card.isFaceUp)
-
-        // ✨ GLOW EFFECT WHEN MATCHED
+        .shadow(radius: 4)
         .shadow(
             color: card.isMatched ? card.color.opacity(0.9) : .clear,
-            radius: card.isMatched ? 12 : 0
+            radius: card.isMatched ? 14 : 0
         )
-        .scaleEffect(card.isMatched ? 1.05 : 1.0)
+        .scaleEffect(card.isMatched ? 1.05 : 1)
         .animation(.spring(response: 0.5), value: card.isMatched)
-
     }
 }
